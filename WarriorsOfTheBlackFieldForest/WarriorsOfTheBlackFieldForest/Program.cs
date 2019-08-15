426b715d1138c61ee01323a2f4f280a0f810e42a
katdan <kd9nlmku8gfe>
2019-08-15 21:36:55 +0200

    odstranene warningy a premenovany Form1

--- WarriorsOfTheBlackFieldForest/WarriorsOfTheBlackFieldForest/Program.cs
+++ WarriorsOfTheBlackFieldForest/WarriorsOfTheBlackFieldForest/Program.cs
@@ -16,7 +16,7 @@ namespace WarriorsOfTheBlackFieldForest
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Hra());
         }
     }
 }
